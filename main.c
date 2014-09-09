351630abd68b3ce2dfe542d664a0ca365b0b3745
Simon Hartcher <simon@simonhartcher.com>
2014-09-09 22:03:20 +1000

    Test code for graph

--- main.c
+++ main.c
@@ -31,6 +31,28 @@ int main (int argc, char *argv[]) {
     graph_add_vertex(&g, &north_v);
     graph_add_vertex(&g, &south_v);
 
+    //print adjacencies
+    linkedlist_t vertices = g.vertices;
+    node_t inode = *vertices.head;
+    for (int i = 0; i < vertices.size; ++i)
+    {
+      vertex_t v = *(vertex_t *)inode.data;
+      printf("%s:\r\n", v.name);
+
+      node_t jnode = *v.adjacent_vertices.head;
+      for (int j = 0; j < v.adjacent_vertices.size; ++j)
+      {
+        vertex_t w = *(vertex_t *)jnode.data;
+        printf("Is connected to %s\r\n", w.name);
+
+        if (jnode.nextptr)
+          jnode = *jnode.nextptr;
+      }
+
+      if (inode.nextptr)
+        inode = *inode.nextptr;
+    }
+
     graph_delete(&g);
   }
 }
