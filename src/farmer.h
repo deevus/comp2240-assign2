30bce70b6f8a5251171aad4380c5f869ecb30a5c
Simon Hartcher <simon@simonhartcher.com>
2014-10-03 23:55:46 +1000

    Added generic cross method for q4.

--- src/farmer.h
+++ src/farmer.h
@@ -1,7 +1,7 @@
 #ifndef FARMER_H
 #define FARMER_H
 
-typedef enum { NORTH, SOUTH } direction;
+typedef enum { NORTH, SOUTH, WEST } direction;
 typedef struct Farmer farmer_t;
 struct Farmer {
   int id;
--- src/question4.c
+++ src/question4.c
@@ -1,6 +1,7 @@
 #include "question4.h"
 #include <stdio.h>
 #include <pthread.h>
+#include <stdbool.h>
 #include "time.h"
 #include "monitor.h"
 #include "semaphore_nostarve.h"
@@ -8,16 +9,45 @@
 
 static int total_crossed_north_south;
 static semaphore_t north_south_bridge;
-static condition_t north_south_bridge_clear;
 static mutex_t lock;
+static bool north_south_in_use;
 
 void run(int north_farmers, int south_farmers) {
 
   sem_ns_init(&north_south_bridge, 1);
-  condition_init(&north_south_bridge_clear);
   mutex_init(&lock);
   common_init(north_farmers, south_farmers, &cross_bridge);
 
+  north_south_in_use = false;
+}
+
+static void cross(farmer_t *farmer, direction from, direction to, bool has_load) {
+	if ((to == NORTH || to == SOUTH) && (from == NORTH || from == SOUTH)) {
+
+		printf("%s farmer %d now %s\r\n", 
+	    (farmer->direction == NORTH ? "North" : "South"), 
+	    farmer->id, 
+	    (has_load ? "crossing" : "returning"));
+
+		int sleep_base = 500;
+		int step_base = 5;
+		int multiplier = has_load ? 2 : 1;
+
+	  msleep(sleep_base * multiplier);
+	  printf("%d steps\r\n", step_base * multiplier);
+
+	  msleep(sleep_base * multiplier);
+	  printf("%d steps\r\n", step_base * multiplier * 2);
+
+	  msleep(sleep_base * multiplier);
+	  printf("%d steps\r\n", step_base * multiplier * 3);	
+
+    printf("%s farmer %d has %s\r\n", 
+      (farmer->direction == NORTH ? "North" : "South"), 
+      farmer->id,
+      (has_load ? "crossed" : "returned"));
+
+	}
 }
 
 void cross_bridge(farmer_t *farmer) {
@@ -27,57 +57,26 @@ void cross_bridge(farmer_t *farmer) {
   //loop forever
   while (1) {
 
-  	monitor_wait(&lock, &north_south_bridge_clear);
     sem_ns_wait(&north_south_bridge);
 
-    printf("%s farmer %d now crossing\r\n", 
-      (farmer->direction == NORTH ? "North" : "South"), 
-      farmer->id);
+    //flag bridge as in use
+    north_south_in_use = true;
 
-    msleep(1000);
-    printf("10 steps\r\n");
-
-    msleep(1000);
-    printf("20 steps\r\n");
-
-    msleep(1000);
-    printf("30 steps\r\n");
+    cross(farmer, NORTH & SOUTH, farmer->direction, true);
 
     total_crossed_north_south++;
 
-    printf("%s farmer %d has crossed\r\n", 
-      (farmer->direction == NORTH ? "North" : "South"), 
-      farmer->id);
-
     sem_ns_signal(&north_south_bridge);
-    monitor_signal(&north_south_bridge_clear);
 
     printf("Total Crossed: %d\r\n", total_crossed_north_south);
 
-    monitor_wait(&lock, &north_south_bridge_clear);
     sem_ns_wait(&north_south_bridge);
 
-    printf("%s farmer %d now returning\r\n", 
-      (farmer->direction == NORTH ? "North" : "South"), 
-      farmer->id);
-
-    msleep(500);
-    printf("5 steps\r\n");
-
-    msleep(500);
-    printf("10 steps\r\n");
-
-    msleep(500);
-    printf("15 steps\r\n");
+    cross(farmer, NORTH & SOUTH, farmer->direction, false);
 
     total_crossed_north_south++;
 
-    printf("%s farmer %d has returned\r\n", 
-      (farmer->direction == NORTH ? "North" : "South"), 
-      farmer->id);
-
     sem_ns_signal(&north_south_bridge);
-    monitor_signal(&north_south_bridge_clear);
 
     printf("Total Crossed: %d\r\n", total_crossed_north_south);
   }
